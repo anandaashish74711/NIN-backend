f2858580d00e695511e271df7937015e7bf7d43a
anandaashish74711 <anandaashish512@gmail.com>
2024-09-21 19:28:58 +0530

    cors error

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 // server.js or app.js
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const body_parser_1 = __importDefault(require("body-parser"));
 const ehrRouter = require('./src/routes/ehrRoutes');
-const bodyParser = require('body-parser');
 const patientRoutes = require('./src/routes/PatientRoute');
 const visitroute = require('./src/routes/VisitRoute');
 const clinicalRoute = require('./src/routes/clinicalRoute');
@@ -14,10 +15,12 @@ const app = (0, express_1.default)();
 const PORT = process.env.PORT || 3002;
 // Middleware to parse JSON bodies
 app.use(express_1.default.json());
+// Enable CORS
+app.use((0, cors_1.default)());
 app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
-app.use(bodyParser.json());
+app.use(body_parser_1.default.json());
 app.use('/api', patientRoutes);
 app.use('/api', ehrRouter);
 app.use('/api', visitroute);
--- index.ts
+++ index.ts
@@ -1,16 +1,20 @@
 // server.js or app.js
 import express, { Request, Response } from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
 const ehrRouter = require('./src/routes/ehrRoutes');
-const bodyParser = require('body-parser');
 const patientRoutes = require('./src/routes/PatientRoute');
 const visitroute = require('./src/routes/VisitRoute');
-const clinicalRoute=require('./src/routes/clinicalRoute')
+const clinicalRoute = require('./src/routes/clinicalRoute');
 
 const app = express();
 const PORT = process.env.PORT || 3002;
 
 // Middleware to parse JSON bodies
 app.use(express.json());
+
+// Enable CORS
+app.use(cors());
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello, World!');
@@ -21,8 +25,7 @@ app.use(bodyParser.json());
 app.use('/api', patientRoutes);
 app.use('/api', ehrRouter);
 app.use('/api', visitroute);
-app.use('/api',clinicalRoute);
-
+app.use('/api', clinicalRoute);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
